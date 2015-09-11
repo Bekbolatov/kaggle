d21e2fa7a7847962a1463e6864b7c2b13cbc4984
Renat Bekbolatov <rbekbolatov@avvo.com>
2015-09-11 11:28:08 -0700

    add soup to filecache - sorry...

--- events/native/file_cache.py
+++ events/native/file_cache.py
@@ -1,12 +1,18 @@
 import boto3
 import os.path
-
+from bs4 import BeautifulSoup as bs
 
 class FileCache:
     def __init__(self, loc = '/home/ec2-user/data/cache/'):
         self.loc = loc
         self.s3_key = 'kaggle/native/orig/'
         self.client = boto3.client('s3')
+
+    def get_soup(self, filename):
+        file_handle = self.get_file(filename)
+        file_content = file_handle.read()
+        file_handle.close()
+        return bs(file_content)
 
     def get_file(self, filename):
         if not os.path.isfile(self.loc + filename):
