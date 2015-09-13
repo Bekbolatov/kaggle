257740e5f3b8799ca2b7b33b1cd3cd57450a8576
Renat Bekbolatov <rbekbolatov@avvo.com>
2015-09-13 13:21:05 -0700

    sync

--- events/native/read_sqs.py
+++ events/native/read_sqs.py
@@ -9,37 +9,44 @@ dns_name = dns_name_req.text
 
 sqs = boto3.resource('sqs')
 queue = sqs.get_queue_by_name(QueueName='cluster_task_queue')
-
 new_member_queue = sqs.get_queue_by_name(QueueName='cluster_new_member')
-new_member_queue.send_message(MessageBody='new_member', MessageAttributes={
-    'Worker': {
-        'StringValue': dns_name,
-        'DataType': 'String'
-        }
-    })
-
-# Committing to tasks
 commitment_queue = sqs.get_queue_by_name(QueueName='cluster_task_commitment')
+
+def send_dns_to_queue(receiving_queue, msg):
+    receiving_queue.send_message(MessageBody=msg, MessageAttributes={
+        'Worker': {
+            'StringValue': dns_name,
+            'DataType': 'String'
+            }
+        })
+
+def send_to_member_queue(msg):
+    send_dns_to_queue(new_member_queue, msg)
+
+def send_to_commit_queue(msg):
+    send_dns_to_queue(commitment_queue, msg)
+
+def log(msg):
+    with open('/var/log/renat_cluster/daemon.log', 'a') as f:
+        f.write(msg + '\n')
+
+
+send_to_member_queue('new_member')
 
 keep_receiving = True
 while keep_receiving:
     for message in queue.receive_messages():
         msg = message.body
-        with open('/var/log/renat_cluster/daemon.log', 'a') as f:
-            f.write('Received: {0}\n'.format(msg))
-        commitment_queue.send_message(MessageBody=msg, MessageAttributes={
-            'Worker': {
-                'StringValue': dns_name,
-                'DataType': 'String'
-                }
-            })
-        message.delete()
+        log('Received: {0}'.format(msg))
         if msg == 'quit':
             keep_receiving = False
+            log('Quitting')
+            send_to_member_queue('exit_member')
             break
         if msg.startswith('parse:'):
-            with open('/var/log/renat_cluster/daemon.log', 'a') as f:
-                f.write('Processing parsing task: {0}\n'.format(msg))
+            send_to_commit_queue(msg)
+            message.delete()
+            log('Processing parsing task: {0}'.format(msg))
             parser.run(msg[6:])
 
 
