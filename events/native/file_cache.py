9b6ad93cff12a6cc2efce9ff5653f347b351b9a8
Renat Bekbolatov <rbekbolatov@avvo.com>
2015-09-13 11:42:15 -0700

    make DocProcessor

--- events/native/file_cache.py
+++ events/native/file_cache.py
@@ -1,52 +1,77 @@
 import boto3
 import os, os.path
 from bs4 import BeautifulSoup as bs
+import json
 
 class FileCache:
 
-    def __init__(self, local_cache_loc='/home/ec2-user/data/cache/', s3_key_prefix='kaggle/native/orig/', max_kb=3145728):
+    def __init__(self,
+            local_cache_loc='/home/ec2-user/data/cache/',
+            s3_key_prefix='kaggle/native/',
+            run_name='docs01',
+            max_kb=3145728):
         """ max_kb 3145728 = 3 GB *1024 * 1024"""
-        self.local_cache_loc = local_cache_loc
-        self.s3_key_prefix = s3_key_prefix
+        self.s3_key_prefix = s3_key_prefix + 'orig/'
+        self.s3_key_prefix_upload = s3_key_prefix + run_name + '/'
         self.client = boto3.client('s3')
         self.bucket = 'sparkydotsdata'
-        self.max_kb = max_kb
+        self.download_cache = os.path.join(local_cache_loc, 'download/')
+        self.upload_cache = os.path.join(local_cache_loc, 'upload/')
+        self.max_kb_download = int(max_kb*0.8)
+        self.max_kb_upload = max_kb - self.max_kb_download
 
-    def get_file(self, filename):
-        if not os.path.isfile(self.local_cache_loc + filename):
+    def open_file(self, filename):
+        if not os.path.isfile(os.path.join(self.download_cache, filename)):
             self.download_file(filename)
-        return open(self.local_cache_loc + filename)
+        return open(os.path.join(self.download_cache, filename))
 
     def download_file(self, filename):
-       self.clean_cache()
-       self.client.download_file(self.bucket, self.s3_key_prefix + filename , self.local_cache_loc + filename) 
+       self.clean_download_cache()
+       self.client.download_file(self.bucket, self.s3_key_prefix + filename, os.path.join(self.download_cache, filename))
 
-    def clean_cache(self):
-        print("Used space: %d KB." % (used_kb))
-        used_kb = self.current_cache_size()
-        if used_kb > self.max_kb:
+    def upload_file(self, filename, contents):
+       self.clean_upload_cache()
+       self.save_file(os.path.join(self.upload_cache, filename), contents)
+       self.client.upload_file(os.path.join(self.upload_cache, filename), self.bucket, self.s3_key_prefix_upload + filename) 
+
+    def save_file(self, location, json_array):
+        with open(location, mode='w') as feedsjson:
+            for entry in json_array:
+                json.dump(entry, feedsjson)
+                feedsjson.write('\n')
+        feedsjson.close()
+
+    def clean_download_cache(self):
+        self.clean_cache(self.download_cache, self.max_kb_download)
+
+    def clean_upload_cache(self):
+        self.clean_cache(self.upload_cache, self.max_kb_upload)
+
+    def clean_cache(self, path, limit_kb):
+        used_kb = self.current_cache_size(path)
+        print("Used space: %d KB in path: %s" % (used_kb, path))
+        if used_kb > limit_kb:
             print("Cache reached max_kb size - clearing cache")
-            files = self.sorted_ls()
+            files = self.sorted_ls(path)
             earlier_files = files[:(len(files)//2)]
+            later_files = files[(len(files)//2):]
             print("First trying to remove only earlier half")
             for filename in earlier_files:
-                os.remove(self.local_cache_loc + filename)
-            if self.current_cache_size() > self.max_kb:
-                later_files = files[(len(files)//2):]
+                os.remove(os.path.join(path, filename))
+            if self.current_cache_size(path) > limit_kb:
                 print("... not enough, removing all")
                 for filename in later_files:
-                    os.remove(self.local_cache_loc + filename)
+                    os.remove(os.path.join(path, filename))
 
-    def sorted_ls(self):
-        path = self.local_cache_loc
+    def sorted_ls(self, path):
         mtime = lambda f: os.stat(os.path.join(path, f)).st_mtime
         return list(sorted(os.listdir(path), key=mtime))
 
-    def current_cache_size(self):
-        used_kb = sum([os.path.getsize(self.local_cache_loc + filename) for filename in os.listdir(self.local_cache_loc)]) // 1024
+    def current_cache_size(self, path):
+        used_kb = sum([os.path.getsize(os.path.join(path, filename)) for filename in os.listdir(path)]) // 1024
         return used_kb
 
-class SoupReader:
+class SoupIO:
 
     def __init__(self, local_cache_loc='/home/ec2-user/data/cache/', s3_key_prefix='kaggle/native/orig/',max_kb=3145728):
         self.file_cache = FileCache(local_cache_loc=local_cache_loc, s3_key_prefix=s3_key_prefix,max_kb=max_kb)
@@ -55,24 +80,37 @@ class SoupReader:
         file_handle = self.file_cache.get_file(filename)
         file_content = file_handle.read()
         file_handle.close()
-        return bs(file_content, 'lxml')
-
-class SoupIterator:
-    def __init__(self, filenames):
-        self.filenames = filenames
-        self.index = 0
-        self.soup_reader = SoupReader()
-
-    def __iter__(self):
-        self.index = 0
-        return self
-
-    def next(self):
-        if self.index >= len(self.filenames):
-            raise StopIteration
+        if file_content:
+            soup = bs(file_content, 'lxml')
         else:
-            value = self.filenames[self.index]
-            self.index += 1
-            return self.soup_reader.get_soup(value)
+            soup = None
+        return soup
+
+    def put_docs(self, filename, docs_array):
+        self.file_cache.upload_file(filename, docs_array)
+
+class DocProcessor:
+    def __init__(self, filenames, log_dir='/home/ec2-user/logs/'):
+        self.filenames = filenames
+        self.log_dir = log_dir
+        self.soup_io = SoupIO()
+
+    def process(self, parse, part_id):
+        ferr = open(os.path.join(log_dir, "errors_in_scraping.log"),"w")
+        json_array = []  
+        for i, filename in enumerate(self.filenames):
+            soup = self.soup_io.get_soup(filename)
+            if soup:
+                try:
+                    doc = parse(soup)
+                    json_array.append(doc)
+                except Exception as e:
+                    ferr.write("parse error with reason : "+str(e)+" on page "+urlId+"\n")
+            if i % 100 == 0:
+                print("processed %d docs" % (i))
+
+        self.soup_io.put_doc(part_id, doc)
+        ferr.close()
+
 
 
