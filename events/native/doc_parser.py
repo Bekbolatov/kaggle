9994cd02f00974981d1ac8d7298ce6f2ea707faf
Renat Bekbolatov <rbekbolatov@avvo.com>
2015-09-13 15:10:22 -0700

    add git

--- events/native/doc_parser.py
+++ events/native/doc_parser.py
@@ -1,6 +1,36 @@
+import re
+
+
+def get_paragraphs(soup):
+    paragraphs = soup.findAll('p')
+    cleaned_texts = [re.sub(r'[\'"|\n\t,.:;()\-\/]+', ' ', p.text.encode('ascii', 'ignore').strip()) for p in paragraphs]
+    return cleaned_texts 
+
+def get_title(soup):
+    title = soup.find('title')
+    title = '' if not title else title.text
+    title = 'long_title' if len(title) > 200 else title.encode('ascii', 'ignore').strip().replace('\n',' ')
+    return title
+
+def get_links(soup):
+    hrefs = []
+    texts = []
+    links = soup.findAll('a')
+    hrefs = [a.href for a in links]
+    texts = [re.sub(r'[\'"|\n\t,.:;()\-\/]+', ' ', a.text.encode('ascii', 'ignore').strip()) for a in links]
+    return hrefs, texts
 
 
 def parse(soup, filename):
-    title = soup.title.text
-    return {"id": filename, "title": title} 
+    title = get_title(soup)
+    pars = get_paragraphs(soup)
+    ahrefs, atexts = get_links(soup)
+    doc = {
+        "id": filename, 
+        "title": title,
+        "pars": pars,
+        "ahrefs": ahrefs,
+        "atexts": atexts
+        }
+    return doc
 
