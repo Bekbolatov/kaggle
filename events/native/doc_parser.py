c436a30ece9829c28558d15b88142426393e8821
Renat Bekbolatov <rbekbolatov@avvo.com>
2015-09-13 19:36:45 -0700

    sync

--- events/native/doc_parser.py
+++ events/native/doc_parser.py
@@ -1,39 +1,46 @@
 import re
 
+def clean_text(item):
+    item = re.sub(r'[\'"|\n\t,.:;()\-\/]+', ' ', item.encode('ascii', 'ignore').strip()).lower()
+    item = re.sub(r'\s+', ' ', item)
+    return item
+
+def clean_texts(items):
+    items = [clean_text(item) for item in items]
+    return items
 
 def get_paragraphs(soup):
     paragraphs = soup.find_all('p')
-    cleaned_texts = [re.sub(r'[\'"|\n\t,.:;()\-\/]+', ' ', p.text.encode('ascii', 'ignore').strip()).lower() for p in paragraphs]
-    cleaned_texts = [re.sub(r'\s+', ' ', text) for text in cleaned_texts]
+    cleaned_texts = clean_texts([p.text for p in paragraphs])
     return cleaned_texts 
 
-def get_images(soup):
+def get_image_srcs(soup):
     images = soup.find_all('img')
     srcs = [image.get('src') for image in images]
     return srcs 
 
-def get_scripts(soup):
+def get_script_srcs(soup):
     scripts = soup.find_all('script')
     srcs = [script.get('src') for script in scripts]
     return srcs 
 
-def get_styles(soup):
+def get_style_srcs(soup):
     styles = soup.find_all('style')
     srcs = [style.get('src') for style in styles]
     return srcs 
 
 def get_title(soup):
     title = soup.find('title')
-    title = '' if not title else title.text
-    title = 'longtitle' if len(title) > 200 else title.encode('ascii', 'ignore').strip().replace('\n',' ')
+    title = 'e' if not title else title.text
+    title = 'longtitle' if len(title) > 300 else clean_text(title)
     return title
 
 def get_links(soup):
     hrefs = []
     texts = []
-    links = soup.findAll('a', href=True)
+    links = soup.find_all('a', href=True)
     hrefs = [a['href'] for a in links]
-    texts = [re.sub(r'[\'"|\n\t,.:;()\-\/]+', ' ', a.text.encode('ascii', 'ignore').strip()) for a in links]
+    texts = clean_texts([a.text for a in links])
     return hrefs, texts
 
 
