7000b9d4c8bbfd2ef86470d668cd86838ad55c07
Renat Bekbolatov <rbekbolatov@avvo.com>
2015-09-16 13:47:46 -0700

    simplify parse doc, focusing only on text

--- events/native/doc_parser.py
+++ events/native/doc_parser.py
@@ -1,7 +1,9 @@
 from __future__ import division
 import re
-from parsing_url import a2text
+from urlparse import urlparse
+import collections
 
+foreign_domain_pattern = re.compile(r'^[^\.]{2,3}\.[^\.]{2}$')
 word_splitter = re.compile(r'[a-z]{4,}')
 
 def clean_text(dirty_text):
@@ -13,12 +15,68 @@ def clean_text(dirty_text):
     clean_text = ' '.join(tokens)
     return clean_text
 
-def parse(soup, filename):
-    tag_p = [item.text.encode('ascii', 'ignore') for item in soup.find_all('p')]
-    tag_title = [item.text.encode('ascii', 'ignore') for item in soup.find_all('title')]
-    tag_a = [a2text(item) for item in soup.find_all('a', href=True)]
+social_domain_names = [
+    'twitter.com',
+    'facebook.com',
+    'google.com',
+    'pinterest.com',
+    'linkedin.com',
+    'reddit.com',
+    'olark.com',
+    'disqus.com',
+    'apple.com',
+    'yelp.com',
+    'imgur.com',
+    'instagram.com',
+    'youtube.com',
+    'tumblr.com',
+    'feedburner.com',
+    ]
 
-    text = clean_text(tag_p + tag_title + tag_a)
+def clean_domain(host):
+    try:
+        num_pieces = host.count('.') + 1
+        if num_pieces <= 2:
+            return host
+        shortened_host = host[(host.find('.')+1):]
+        if num_pieces == 3: 
+            if foreign_domain_pattern.match(shortened_host):
+                return host
+            else:
+                return shortened_host
+        more_shortened_host = shortened_host[(shortened_host.find('.')+1):]
+        if foreign_domain_pattern.match(more_shortened_host):
+            return shortened_host
+        else:
+            return more_shortened_host        
+    except Exception as inst:
+        print inst
+        print href
+        return 'bad_bad_domain'
+
+def a2text(a):
+    try:
+        ahref = a['href']
+        atext = a.text.encode('ascii', 'ignore')
+        parsed_url = urlparse(ahref)
+        raw_domain = parsed_url.netloc
+        if raw_domain in social_domain_names:
+            return ''
+        raw_path = parsed_url.path
+        domain = clean_domain(raw_domain)
+        text = raw_path + ' ' + atext
+        return domain + ' ' + clean_text(text)
+    except Exception as inst:
+        print inst
+        return 'bad_bad_domain'
+
+
+def parse(soup, filename):
+    tag_p = clean_text([item.text.encode('ascii', 'ignore') for item in soup.find_all('p')])
+    tag_title = clean_text([item.text.encode('ascii', 'ignore') for item in soup.find_all('title')])
+    tag_a = ' '.join([a2text(item) for item in soup.find_all('a', href=True)])
+
+    text = ' '.join([tag_p, tag_title, tag_a])
 
     return {
         "id": filename, 
