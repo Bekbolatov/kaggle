2096607c76bf681459922d8d9bb42e1db01853c0
Renat Bekbolatov <rbekbolatov@avvo.com>
2015-09-16 14:45:43 -0700

    simplify parse doc, focusing only on text

--- events/native/word_stemmer.py
+++ events/native/word_stemmer.py
@@ -1,25 +1,18 @@
+from nltk.stem.porter import PorterStemmer
+stemmer = PorterStemmer()
 
 def stem(word):
     if not word:
         return ''
     if len(word) < 4:
         return word
-    if word.endswith('ly'):
-        return stem(word[:-2]) # exactly -> exact
-    if word.endswith('s') or word.endswith('e') or word.endswith('y'): 
-        return stem(word[:-1]) # books -> book, create -> creat, assembly -> assembl
-    if word.endswith('ment'):
-        return stem(word[:-4]) #establishment -> establish
-    if word.endswith('tion'):
-        return stem(word[:-3]) #constriction -> constrict
-    if word.endswith('ed') or word.endswith('or') or word.endswith('er'):
-        return stem(word[:-2]) # started -> start, creator -> creat, builder -> build
+
+    word = stemmer.stem(word)
+
     if word.endswith('ea') or word.endswith('ia'):
-        return stem(word[:-2]) # russia -> russ, asia -> as
+        return word[:-2] # russia -> russ, asia -> as
     if word.endswith('ean') or word.endswith('ian'):
-        return stem(word[:-3]) # russian -> russ, asian -> as
-    if word.endswith('ful'):
-        return stem(word[:-3]) # powerful, neglectful -> power, neglect
-    if word.endswith('ies'):
-        return stem(word[:-3]) # responsibilities -> responsibilit
+        return word[:-3] # russian -> russ, asian -> as
+
     return word
+
