f557d4379397a4fd6e06e8ba9a323428469e5b13
Renat Bekbolatov <rbekbolatov@avvo.com>
2015-09-22 16:52:36 -0700

    trying GB with GraphLab depth 6, step size 0.2, max_iter=400, cols 0.40, makes 0.92 AUC and 89.5/84.6 ACC

--- notes/ec2-GraphLab-instance.py
+++ notes/ec2-GraphLab-instance.py
@@ -13,10 +13,12 @@ c = 'subnet-ac9314f5'
 
 volume = 'vol-24e7f9ea'
 
+image = 'ami-d3c3dae3'  # GraphLab-1.1
+older_image = 'ami-45afb075' # GRaphLab 1.1   //ami-bd6e718d'(GraphLab 1.0)
 
 conn = boto.ec2.connect_to_region("us-west-2")
 conn.request_spot_instances(price='1.50', 
-        image_id='ami-45afb075', # GraphLab 1.1      #'ami-bd6e718d'(GraphLab 1.0)
+        image_id=image,
         count=1, 
         type="one-time", 
         key_name="panerapig",
