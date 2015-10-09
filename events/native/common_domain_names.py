dcbdbbeb484c7ec2511371e5ce23a4b46b4d5a93
Renat Bekbolatov <rbekbolatov@avvo.com>
2015-10-08 22:58:33 -0700

    add top-1m

--- events/native/common_domain_names.py
+++ events/native/common_domain_names.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-top1m = pd.read_csv('data/top-1m.dat', names=['rank', 'domain'], index_col='domain')
+top1m = pd.read_csv('/home/ec2-user/repos/bekbolatov/kaggle/events/native/data/top-1m.dat', names=['rank', 'domain'], index_col='domain')
 
 def get_rank(domain):
     try:
